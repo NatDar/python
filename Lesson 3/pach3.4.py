46009efc1fe580282685a638734f4f3b17d1d557
DarNat <natali.programmer@gmail.com>
2022-04-01 12:36:23 +0300

    lesson3_6pach

---
+++ Lesson 3/pach3.4.py
@@ -0,0 +1,24 @@
+def power(a, n):
+    res = 1
+    for i in range(abs(n)):
+        res *= a
+    if n >= 0:
+        return res
+    else:
+        return 1 / res
+
+
+print(power(float(input("Первое значение - ")), int(input("Второе значение - "))))
+
+
+
+
+def pow_h(base, degree, module):
+    degree = bin(degree)[2:]
+    r = 1
+
+    for i in range(len(degree) - 1, -1, -1):
+        r = (r ** 2) % module
+        r = (r * base ** int(degree[i])) % module
+    base = (base ** 2) % module
+    return r
