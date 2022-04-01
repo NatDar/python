8a957ff8f0c0f761b59b86f0845a878a8d67c7f5
DarNat <natali.programmer@gmail.com>
2022-04-01 13:02:48 +0300

    lesson3_6pach

--- Lesson 3/pach3.2.py
+++ Lesson 3/pach3.2.py
@@ -7,4 +7,5 @@ telephone = input('input telephone')
 
 def my_func (name, surname, year, city, email, telephone):
      return ' '.join([name, surname, year, city, email, telephone])
-print(my_func(surname = 'surname', name = 'name', year = 'year', city = 'city', email = 'email', telephone = 'telephone'))
+print(my_func(surname = 'Darovskaya', name = 'Natallia', year = '1982', city = 'Zhlobin', email = 'nat@mail.ru', telephone = '8-375-300-99-87')) 
+
